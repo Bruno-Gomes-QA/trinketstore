4e143b3448a00e9f6110a08b47b92661839cfced
Bruno-Gomes-QA <bruno.gomes.qa@gmail.com>
2025-11-22 21:13:31 -0300

    CORS

--- backend/src/main/java/com/reducess/trinketstore/config/SecurityConfig.java
+++ backend/src/main/java/com/reducess/trinketstore/config/SecurityConfig.java
@@ -53,7 +53,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://trinket-store.reducess.com"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
